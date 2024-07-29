51a56964e6c13b14c0908a9c5c80a0297ecc95fe
JaeHyunGround <gothddlek@naver.com>
2024-07-29 22:30:13 +0900

    ✨feat : 사용될 type 지정

---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+export interface LinkTypes {
+  name: string
+  link: string
+}
+
+export interface ProjectTypes {
+  name: string
+  description: string
+  link: string
+}
+
+export interface ActivityTypes {
+  name: string
+  description: string
+  date: string
+}
