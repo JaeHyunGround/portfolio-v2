2fb10b16c212d1d09e5694b65ef34507a1bf7a39
JaeHyunGround <gothddlek@naver.com>
2024-08-03 00:25:55 +0900

    ✨feat : Footer container 제작

---
+++ src/containers/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+  return (
+    <main className={styles.Footer}>
+      Copyright 2024. Jaehyun Ahn Frontend developer portfolio
+    </main>
+  )
+}
