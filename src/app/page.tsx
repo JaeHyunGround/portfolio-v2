59598a311baa0e937cdc4f810d261186fa85d781
JaeHyunGround <gothddlek@naver.com>
2024-08-04 16:01:31 +0900

    ✨feat : Header container 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Activity from '@/components/Activity'
 import Footer from '@/containers/Footer'
+import Header from '@/containers/Header'
 import Info from '@/containers/Info'
 import Main from '@/containers/Main'
 import Projects from '@/containers/Projects'
@@ -7,6 +8,7 @@ import Projects from '@/containers/Projects'
 export default function Home() {
   return (
     <main>
+      <Header />
       <Main />
       <Info />
       <Projects />
